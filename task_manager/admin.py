e0a91c31e57c252bd2067600c0c3917cc4faa9ee
ssani4 <ssani4@icloud.com>
2025-04-07 16:26:48 +0200

    hw 9

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -3,6 +3,27 @@ from django.contrib import admin
 # Register your models here.
 from task_manager.models import Category, Task, SubTask
 
-admin.site.register(Category)
-admin.site.register(Task)
-admin.site.register(SubTask)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'status', 'deadline')
+    search_fields = ('title',)
+    list_filter = ('status', 'deadline')
+
+
+@admin.register(SubTask)
+class SubTaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'status', 'deadline')
+    search_fields = ('title',)
+    list_filter = ('status', 'deadline')
+
+
+# admin.site.register(Category)
+# admin.site.register(Task)
+# admin.site.register(SubTask)
